16a849a5e4870badadac59aa673939887264fe34
Govindan007 <sgovindan077@gmail.com>
2025-06-27 15:01:03 +0530

    third commit

--- studentapp/backend/model.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require("mongoose");
-var studentSchema=mongoose.Schema({
-    name:String,
-    age:Number,
-    course:String,
-    place:String
-})
-var studentmodel = mongoose.model("student", studentSchema)
-module.exports = studentmodel;
